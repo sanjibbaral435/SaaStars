408faba018db37e15812e34f39b4b2d3a912e0d0
EC2 Default User <ec2-user@ip-172-31-11-37.us-west-2.compute.internal>
2018-03-29 05:04:00 +0000

    adding model for projects page

--- animalscience/animalscience/webapp/models.py
+++ animalscience/animalscience/webapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class ProjectsData(models.Model):
+    project_title = models.CharField(max_length=30, default='none')
+    project_author_name = models.CharField(max_length=30, default='none')
+    project_year = models.DateField(null=True)
+    project_keywords = models.CharField(max_length=30, default='none')
+    def __str__(self):              # __unicode__ on Python 2
+        return self.project_title
